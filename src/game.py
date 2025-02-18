483501c35cabfd1406106f27ed65c2d480fa0bd4
Shallange <jimmy.kroneld@hotmail.com>
2025-02-18 11:38:55 +0100

    Moved Print_status,inventory and score to player - added comments

--- src/game.py
+++ src/game.py
@@ -3,30 +3,18 @@ from .player import Player
 from . import pickups
 
 
-
-player = Player(2, 1)
-score = 0
-inventory = []
-
-g = Grid()
-g.set_player(player)
-g.make_walls()
-pickups.randomize(g)
-
-
-# TODO: flytta denna till en annan fil
-def print_status(game_grid):
-    """Visa spelvärlden och antal poäng."""
-    print("--------------------------------------")
-    print(f"You have {score} points.")
-    print(game_grid)
+#----------Skapa player, spelplanen och items----------
+player = Player(17, 6)# start x and y pos for player
+g = Grid()#skapa spelplanen
+g.set_player(player)# placera player på spelplanen
+g.make_walls()#skapa spelplanets väggar
+pickups.randomize(g)#random placering av items på spelplanen
 
 
 command = "a"
 # Loopa tills användaren trycker Q eller X.
 while not command.casefold() in ["q", "x"]:
-    print_status(g)
-
+    player.print_status(g)
     command = input("Use WASD to move, Q/X to quit. ")
     command = command.casefold()[:1]
 
@@ -37,7 +25,7 @@ while not command.casefold() in ["q", "x"]:
 
         if isinstance(maybe_item, pickups.Item):
             # we found something
-            score += maybe_item.value
+            player.score += maybe_item.value
             print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
             #g.set(player.pos_x, player.pos_y, g.empty)
             g.clear(player.pos_x, player.pos_y)
--- src/player.py
+++ src/player.py
@@ -1,9 +1,12 @@
 class Player:
-    marker = "@"
+    marker = "@"#Spelarens ikon på spelplanen
 
     def __init__(self, x, y):
         self.pos_x = x
         self.pos_y = y
+        self.score = 0
+        self.inventory = []
+
 
     # Flyttar spelaren. "dx" och "dy" är skillnaden
     def move(self, dx, dy):
@@ -13,8 +16,14 @@ class Player:
         self.pos_x += dx
         self.pos_y += dy
 
+
     def can_move(self, x, y, grid):
         return True
         #TODO: returnera True om det inte står något i vägen
 
 
+    def print_status(self, game_grid):
+        """Visa spelvärlden och antal poäng."""
+        print("--------------------------------------")
+        print(f"You have {self.score} points.")
+        print(game_grid)
