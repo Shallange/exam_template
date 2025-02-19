07a2ef3c08a7e02834cc29bcf076dc723b9325f9
Shallange <jimmy.kroneld@hotmail.com>
2025-02-19 11:32:12 +0100

    Moved player movement and itemcheck into player, added wall collision and inventory

--- src/game.py
+++ src/game.py
@@ -11,25 +11,15 @@ g.make_walls()#skapa spelplanets väggar
 pickups.randomize(g)#random placering av items på spelplanen
 
 
-command = "a"
+command = "t"
 # Loopa tills användaren trycker Q eller X.
 while not command.casefold() in ["q", "x"]:
     player.print_status(g)
     command = input("Use WASD to move, Q/X to quit. ")
     command = command.casefold()[:1]
+    player.direction(command,g,pickups)
+    player.show_inventory(command)
+    
+ 
 
-    if command == "d" and player.can_move(1, 0, g):  # move right
-        # TODO: skapa funktioner, så vi inte behöver upprepa så mycket kod för riktningarna "W,A,S"
-        maybe_item = g.get(player.pos_x + 1, player.pos_y)
-        player.move(1, 0)
-
-        if isinstance(maybe_item, pickups.Item):
-            # we found something
-            player.score += maybe_item.value
-            print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
-            #g.set(player.pos_x, player.pos_y, g.empty)
-            g.clear(player.pos_x, player.pos_y)
-
-
-# Hit kommer vi när while-loopen slutar
 print("Thank you for playing!")
--- src/player.py
+++ src/player.py
@@ -8,6 +8,14 @@ class Player:
         self.inventory = []
 
 
+    def show_inventory(self,command):
+        if command =="i":
+            if self.inventory != None:
+                for item in self.inventory:
+                    print(f"{item.symbol} - {item.name}")
+            else:
+                print("inventory empty")
+
     # Flyttar spelaren. "dx" och "dy" är skillnaden
     def move(self, dx, dy):
         """Flyttar spelaren.\n
@@ -18,8 +26,34 @@ class Player:
 
 
     def can_move(self, x, y, grid):
+        if grid.get(x, y) == "\u25A0": # \u25A0 = ■
+            return False
         return True
-        #TODO: returnera True om det inte står något i vägen
+
+    def direction(self, command,grid, pickups):
+        dx = 0 
+        dy = 0
+
+        if command == "w":
+            dy = -1 # (0, dy)
+        elif command == "a":
+            dx = -1 # (dx, 0)
+        elif command == "s":
+            dy = 1 # (0, dy)
+        elif command == "d":
+            dx = 1 # (dx, 0)
+        
+        if self.can_move(self.pos_x + dx , self.pos_y + dy,grid):  #här lägger man self och nästa steg om de är en vägg
+            maybe_item = grid.get(self.pos_x + dx , self.pos_y + dy)
+            self.move(dx, dy)
+            self.score -= 1
+            if isinstance(maybe_item, pickups.Item):
+            #we found something
+                self.inventory.append(maybe_item)
+                self.score += maybe_item.value
+                print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
+                grid.set(self.pos_x, self.pos_y, grid.empty)
+                grid.clear(self.pos_x, self.pos_y)
 
 
     def print_status(self, game_grid):
